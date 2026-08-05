29181c024333b64b6c8976b921567734af35fb81
weaponsforge <forgeweapons@gmail.com>
2026-08-05 21:22:12 +0800

    fix: use ROOT_DOMAIN env variable in code

--- docsapp/src/lib/constants.ts
+++ docsapp/src/lib/constants.ts
@@ -16,4 +16,4 @@ export const gitConfig = {
 export const baseUrl =
   process.env.NODE_ENV === 'development' || !process.env.BASE_URL
     ? new URL('http://localhost:3000')
-    : new URL(`https://${process.env.BASE_URL}`)
+    : new URL(`https://${process.env.ROOT_DOMAIN}`)
