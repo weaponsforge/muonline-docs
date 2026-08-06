5f26edc2942548160d9da7722878c00f7b8a9f1e
weaponsforge <forgeweapons@gmail.com>
2026-08-06 19:08:28 +0800

    fix: wrap client components running usesearchparams in suspense

--- docsapp/src/app/auth/layout.tsx
+++ docsapp/src/app/auth/layout.tsx
@@ -1,7 +1,11 @@
+import { Suspense } from 'react'
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="w-full h-screen grid place-content-center bg-neutral-200">
-      {children}
+      <Suspense fallback={null}>
+        {children}
+      </Suspense>
     </div>
   )
 }
